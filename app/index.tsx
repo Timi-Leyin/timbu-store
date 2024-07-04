5754fb32d2d6dd50a05a8aa4e9a5a1aa54e2a3fb
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-04 16:42:03 -0700

    style: onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "@/components/themed/themed-text";
+import { ThemedButton } from "@/components/themed/themed-button";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <ImageBackground source={require("../assets/images/onboarding-stripes.png")} style={styles.container}>
+        <View style={styles.infoBox}>
+          <ThemedText style={styles.infoBoxHeading} type="title">
+            Your Fashion Companion for All Things Trendy
+          </ThemedText>
+          <ThemedButton>Continue</ThemedButton>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    height: "100%",
+    flex: 1,
+  },
+  container: {
+    height: "100%",
+    padding: 30,
+  },
+  infoBoxHeading: {
+    textAlign: "center",
+  },
+  infoBox: {
+    marginTop: "auto",
+  },
+});
