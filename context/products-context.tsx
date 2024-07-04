6c0041c049a9c8fb4459083905c3e4fea5c3030a
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-04 16:54:13 -0700

    feat: product context (INCOMPLETE)

--- context/products-context.tsx
+++ context/products-context.tsx
@@ -1,4 +1,7 @@
-import { createContext } from "react";
+import { GET_PRODUCTS } from "@/api/products";
+import { useFetch } from "@/hooks/use-fetch";
+import { ReactNode, createContext, useEffect } from "react";
+import { Text } from "react-native";
 
 export interface ProductContext {
   loading: boolean;
@@ -7,3 +10,21 @@ export interface ProductContext {
 const productContext = createContext<ProductContext>({
   loading: true,
 });
+
+export const ProductsProvider = ({ children }: { children: ReactNode }) => {
+  const { loading, data, error, fetchData } = useFetch(GET_PRODUCTS);
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+  return (
+    <productContext.Provider
+      value={{
+        loading,
+      }}
+    >
+      {children}
+      {/* {loading?<Text>Loading...</Text>:<Text>Loaded</Text>} */}
+    </productContext.Provider>
+  );
+};
