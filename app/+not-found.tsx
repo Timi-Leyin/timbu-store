e9a6d81e4698e8788ae4af0e8b766dae6976b3ab
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-04 14:12:18 -0700

    fix: themed component imports

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,8 +1,7 @@
+import { ThemedText } from '@/components/themed/themed-text';
+import { ThemedView } from '@/components/themed/themed-view';
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
