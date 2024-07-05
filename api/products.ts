500082ce35c7b3411f046f647884680178632516
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-05 07:38:24 -0700

    feat: get products from timbu

--- api/products.ts
+++ api/products.ts
@@ -1,23 +1,6 @@
 import { api } from "@/config/api";
 import { ENV } from "@/constants/env";
-
-interface GetProducts {
-  reverse_sort?: boolean;
-  page?: number;
-  size?: number;
-}
-
-interface Products {}
-
-export interface GetProductsResponse {
-  page: number;
-  size: number;
-  total: number;
-  debug: unknown;
-  previous_page?: string | null;
-  next_page?: string | null;
-  items: Products[];
-}
+import { GetProducts } from "@/interfaces/products";
 
 export const GET_PRODUCTS = ({
   reverse_sort = false,
@@ -25,6 +8,5 @@ export const GET_PRODUCTS = ({
   size = 10,
 }: GetProducts) => {
   const URL = /** ====> BASE URL ++++ **/ `/products?organization_id=${ENV.TIMBU_ORG_ID}&reverse_sort=${reverse_sort}&page=${page}&size=${size}&Appid=${ENV.TIMBU_APP_ID}&Apikey=${ENV.TIMBU_API_KEY} `;
-
   return api.get(URL);
 };
